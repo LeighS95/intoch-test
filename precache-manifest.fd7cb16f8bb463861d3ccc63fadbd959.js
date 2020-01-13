2459c61adfd6d5f0cc521a25791401c0c7ead2ef
Stephen Leigh <stephen.leigh@verse.co.uk>
2020-01-13 02:47:22 +0000

    Updates

---
+++ precache-manifest.fd7cb16f8bb463861d3ccc63fadbd959.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44bd0005233c529a94513a8566a1572b",
+    "url": "/intoch-test/index.html"
+  },
+  {
+    "revision": "07b35884563405e999df",
+    "url": "/intoch-test/static/css/main.32b8bb97.chunk.css"
+  },
+  {
+    "revision": "4f7e3247e774b0d282a1",
+    "url": "/intoch-test/static/js/2.ff58c1a6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/intoch-test/static/js/2.ff58c1a6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "07b35884563405e999df",
+    "url": "/intoch-test/static/js/main.e45ffb2c.chunk.js"
+  },
+  {
+    "revision": "595f2d1dbe151008d2c7",
+    "url": "/intoch-test/static/js/runtime-main.b2b1c712.js"
+  }
+]);
